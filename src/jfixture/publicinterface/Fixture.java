11d9024ff8381e1e1b54bad66ff469b1896e3a3d
grzesiek-galezowski <grzesiek.galezowski@gmail.com>
2014-02-22 23:35:25 +0100

    a

--- src/jfixture/publicinterface/Fixture.java
+++ src/jfixture/publicinterface/Fixture.java
@@ -1,15 +1,25 @@
 package jfixture.publicinterface;
 
-import java.lang.Class;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.LinkedList;
-import java.util.List;
+
+import jfixture.publicinterface.generators.ArrayGenerator;
+import jfixture.publicinterface.generators.BigDecimalGenerator;
+import jfixture.publicinterface.generators.BigIntGenerator;
+import jfixture.publicinterface.generators.BooleanGenerator;
+import jfixture.publicinterface.generators.BuiltInCollectionGenerator;
+import jfixture.publicinterface.generators.ByteAndCharGenerator;
+import jfixture.publicinterface.generators.CalendarGenerator;
+import jfixture.publicinterface.generators.ConcreteObjectGenerator;
+import jfixture.publicinterface.generators.DateGenerator;
+import jfixture.publicinterface.generators.DoubleGenerator;
+import jfixture.publicinterface.generators.EnumGenerator;
+import jfixture.publicinterface.generators.InstanceGenerator;
+import jfixture.publicinterface.generators.IntGenerator;
+import jfixture.publicinterface.generators.PlainObjectGenerator;
+import jfixture.publicinterface.generators.StringGenerator;
 
 import com.google.common.reflect.TypeToken;
-
-import jfixture.publicinterface.generators.*;
 
 public class Fixture {
 
