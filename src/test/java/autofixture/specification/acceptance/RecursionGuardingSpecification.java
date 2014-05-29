106ccb0bda0ee8e85f5f87c39d4285ceb51a80e8
grzesiek-galezowski <grzesiek.galezowski@gmail.com>
2014-05-29 06:56:55 +0200

    added missing test

--- src/test/java/autofixture/specification/acceptance/RecursionGuardingSpecification.java
+++ src/test/java/autofixture/specification/acceptance/RecursionGuardingSpecification.java
@@ -3,12 +3,23 @@ import autofixture.publicinterface.Fixture;
 import autofixture.specification.acceptance.testfixtures.RecursiveStructure;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 public class RecursionGuardingSpecification {
     private Fixture fixture = new Fixture();
 
     @Test
+    public void shouldHandleRecursiveClasses() {
+        RecursiveStructure instance = fixture.create(RecursiveStructure.class);
+    }
+
+    @Test
     public void shouldReturnNullAsRecursiveObjectWhenItsRecursionIsFinished() {
+        fixture.setRecursionDepth(1);
         RecursiveStructure instance = fixture.create(RecursiveStructure.class);
 
+        assertThat(instance.inner, equalTo(null));
     }
+
 }
