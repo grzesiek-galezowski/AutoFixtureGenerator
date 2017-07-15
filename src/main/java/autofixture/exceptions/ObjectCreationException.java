2f0603483deacda4730419cb3f3982200628bf28
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2017-07-15 12:55:48 +0200

    Refactored strings in exception

--- src/main/java/autofixture/exceptions/ObjectCreationException.java
+++ src/main/java/autofixture/exceptions/ObjectCreationException.java
@@ -2,24 +2,30 @@ package autofixture.exceptions;
 
 
 import autofixture.interfaces.InstanceType;
+import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("serial")
 public class ObjectCreationException extends RuntimeException {
 
   public ObjectCreationException(final InstanceType<?> instanceType) {
-    super("Cannot create an instance of " + instanceType);
+    super(basicMessageFor(instanceType));
   }
 
-  public ObjectCreationException(final InstanceType<?> typeToken, final Exception e) {
-    super("Cannot create an instance of " + typeToken, e);
+  @NotNull
+  private static String basicMessageFor(final InstanceType<?> instanceType) {
+    return "Cannot create an instance of " + instanceType + ".";
   }
 
-  public ObjectCreationException(final InstanceType<?> typeToken, final String additionalDetails) {
-    super("Cannot create an instance of " + typeToken + ". \n" + additionalDetails);
+  public ObjectCreationException(final InstanceType<?> instanceType, final Exception e) {
+    super(basicMessageFor(instanceType), e);
   }
 
-  public ObjectCreationException(final InstanceType<?> typeToken, final String additionalDetails, final Throwable e) {
-    super("Cannot create an instance of " + typeToken + ". \n" + additionalDetails, e);
+  public ObjectCreationException(final InstanceType<?> instanceType, final String additionalDetails) {
+    super(basicMessageFor(instanceType) +" \n" + additionalDetails);
+  }
+
+  public ObjectCreationException(final InstanceType<?> instanceType, final String additionalDetails, final Throwable e) {
+    super(basicMessageFor(instanceType) + "."+" \n" + additionalDetails, e);
   }
 
 
---
+++ src/main/java/autofixture/exceptions/package-info.java
@@ -0,0 +1,5 @@
+@NonnullByDefault
+package autofixture.exceptions;
+
+
+import com.sharneng.annotation.NonnullByDefault;
